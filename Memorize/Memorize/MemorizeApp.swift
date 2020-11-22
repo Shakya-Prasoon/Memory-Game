52e82aac4bb105f9bf930fe0556a5eddf53a14ab
Prasoon Shakya <66377759+Shakya-Prasoon@users.noreply.github.com>
2020-11-22 03:35:41 -0800

    first commit

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Prasoon Shakya on 11/22/20.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
