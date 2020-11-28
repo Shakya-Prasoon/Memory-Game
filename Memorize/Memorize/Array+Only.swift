475fcd048e496aaa5d1860b69b0c6eff65d34781
Prasoon Shakya <66377759+Shakya-Prasoon@users.noreply.github.com>
2020-11-27 23:49:45 -0800

    Array+Only

---
+++ Memorize/Memorize/Array+Only.swift
@@ -0,0 +1,14 @@
+//
+//  Array_Only.swift
+//  Memorize
+//
+//  Created by Prasoon Shakya on 11/27/20.
+//
+
+import Foundation
+
+extension Array {
+    var only: Element? {
+        count == 1 ? first : nil
+    }
+}
